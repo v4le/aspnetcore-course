b1eebdefa37c747aa9736c460c0b3afd23b2432b
vleonardi <valeria.leonardi@codiceplastico.com>
2023-08-18 15:14:55 +0200

    Index: introdotta ricerca

--- DemoApp/DemoApp/Pages/Contatti/Index.cshtml.cs
+++ DemoApp/DemoApp/Pages/Contatti/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DemoApp.Pages.Contatti
@@ -6,9 +7,27 @@ namespace DemoApp.Pages.Contatti
     {
         public IList<Contatto> Contatti { get; set; }
 
+        [BindProperty]
+        public string SearchText { get; set; }
+
         public void OnGet()
         {
             Contatti = ContattiProvider.Contatti;
+        }
+
+        public void OnPost()
+        {
+            if (!string.IsNullOrEmpty(SearchText))
+            {
+                Contatti = ContattiProvider.Contatti.Where(c =>
+                    c.Nome.Contains(SearchText, StringComparison.OrdinalIgnoreCase) ||
+                    c.Cognome.Contains(SearchText, StringComparison.OrdinalIgnoreCase))
+                    .ToList();
+            }
+            else
+            {
+                Contatti = ContattiProvider.Contatti;
+            }
         }
     }
 }
