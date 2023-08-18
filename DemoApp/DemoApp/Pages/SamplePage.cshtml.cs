58217860119611dcf26569ce07bb5761fac6405c
vleonardi <valeria.leonardi@codiceplastico.com>
2023-08-18 09:58:20 +0200

    Aggiunta pagina SamplePage (esempio di pagina con tutti gli elementi)

---
+++ DemoApp/DemoApp/Pages/SamplePage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DemoApp.Pages
+{
+    public class SamplePageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
