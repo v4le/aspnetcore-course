2d45318eeb59b909cd2d9988f1876b2567c11c82
vleonardi <valeria.leonardi@codiceplastico.com>
2023-08-22 14:48:30 +0200

    Index: recupero my connection string (da appsettings)

--- DemoApp/DemoApp/Pages/Index.cshtml.cs
+++ DemoApp/DemoApp/Pages/Index.cshtml.cs
@@ -1,20 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DemoApp.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration configuration;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(IConfiguration configuration)
         {
-            _logger = logger;
+            this.configuration = configuration;
         }
 
         public void OnGet()
         {
-
+            var config = configuration["ConnectionStrings:MyConnectionString"];
         }
     }
 }
