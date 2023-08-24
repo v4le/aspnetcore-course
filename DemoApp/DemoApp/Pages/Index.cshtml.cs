7d526e29abca5ce003a06c3606bb398784bec04d
vleonardi <valeria.leonardi@codiceplastico.com>
2023-08-24 11:45:46 +0200

    Index: aggiunta indicazione environmnet

--- DemoApp/DemoApp/Pages/Index.cshtml.cs
+++ DemoApp/DemoApp/Pages/Index.cshtml.cs
@@ -5,16 +5,21 @@ namespace DemoApp.Pages
     public class IndexModel : PageModel
     {
         private readonly IConfiguration configuration;
-        public string Config { get; set; }
+        private readonly IWebHostEnvironment webHostEnvironment;
 
-        public IndexModel(IConfiguration configuration)
+        public string Config { get; set; }
+        public string Environment { get; set; }
+
+        public IndexModel(IConfiguration configuration, IWebHostEnvironment webHostEnvironment)
         {
             this.configuration = configuration;
+            this.webHostEnvironment = webHostEnvironment;
         }
 
         public void OnGet()
         {
             Config = configuration["ConnectionStrings:MyConnectionString"];
+            Environment = webHostEnvironment.EnvironmentName;
         }
     }
 }
