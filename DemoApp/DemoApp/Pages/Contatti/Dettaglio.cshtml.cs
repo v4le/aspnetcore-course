a8bcd4663d23fcdb996728fd2e9263ff7e861c75
vleonardi <valeria.leonardi@codiceplastico.com>
2023-08-28 13:54:45 +0200

    Caching: aggiunge caching

--- DemoApp/DemoApp/Pages/Contatti/Dettaglio.cshtml.cs
+++ DemoApp/DemoApp/Pages/Contatti/Dettaglio.cshtml.cs
@@ -1,21 +1,38 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace DemoApp.Pages.Contatti
 {
     public class DettaglioModel : PageModel
     {
         private readonly IContattiService contattiService;
+        private readonly IMemoryCache memoryCache;
 
         public Contatto Contatto { get; set; }
 
-        public DettaglioModel(IContattiService contattiService) 
+        public DettaglioModel(IContattiService contattiService, IMemoryCache memoryCache) 
         {
             this.contattiService = contattiService;
+            this.memoryCache = memoryCache;
         }
 
         public void OnGet(int id)
         {
-            Contatto = contattiService.GetContatto(id);
+            if (!memoryCache.TryGetValue($"Contatto_{id}", out Contatto contatto))
+            {
+                // Se i dettagli del contatto non sono nella cache, li carico tramite il servizio
+                contatto = contattiService.GetContatto(id);
+
+                var cacheEntryOptions = new MemoryCacheEntryOptions
+                {
+                    SlidingExpiration = TimeSpan.FromSeconds(20),
+                    //AbsoluteExpiration = new DateTime(2023, 09, 14, 15, 0, 0)
+                };
+
+                memoryCache.Set($"Contatto_{id}", contatto, cacheEntryOptions);
+            }
+
+            Contatto = contatto;
         }
     }
 }
--- DemoApp/DemoApp/Pages/Contatti/Modifica.cshtml.cs
+++ DemoApp/DemoApp/Pages/Contatti/Modifica.cshtml.cs
@@ -1,18 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace DemoApp.Pages.Contatti
 {
     public class ModificaModel : PageModel
     {
         private readonly IContattiService contattiService;
+        private readonly IMemoryCache memoryCache;
 
         [BindProperty]
         public Contatto Contatto { get; set; }
 
-        public ModificaModel(IContattiService contattiService)
+        public ModificaModel(IContattiService contattiService, IMemoryCache memoryCache)
         {
             this.contattiService = contattiService;
+            this.memoryCache = memoryCache;
         }
 
         public void OnGet(int id)
@@ -25,6 +28,8 @@ namespace DemoApp.Pages.Contatti
             if (ModelState.IsValid)
             {
                 var contatto = contattiService.Aggiorna(Contatto);
+                memoryCache.Remove($"Contatto_{contatto.Id}");
+
                 //redirect alla pagina di elenco
                 //return RedirectToPage("/Contatti/Index");
 
